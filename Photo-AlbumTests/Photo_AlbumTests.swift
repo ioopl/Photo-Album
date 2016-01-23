cd7751e90453b56dd1a6fa91859fcf469427e168
UHS <uhs@UHSs-MBP.lan>
2016-01-23 21:06:10 +0000

    Unit Tests Added

--- Photo-AlbumTests/Photo_AlbumTests.swift
+++ Photo-AlbumTests/Photo_AlbumTests.swift
@@ -13,23 +13,87 @@ class Photo_AlbumTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testAlbumsURLConnection() {
+        let URL = NSURL(string: "http://jsonplaceholder.typicode.com/albums")!
+        let expectation = expectationWithDescription("Test Albums URL")
+        
+        let session = NSURLSession.sharedSession()
+        
+        let task = session.dataTaskWithURL(URL) { data, response, error in
+            
+            XCTAssertNotNil(data, "data should not be nil")
+            
+            XCTAssertNil(error, "error should be nil")
+            
+            if let HTTPResponse = response as? NSHTTPURLResponse
+            {
+                XCTAssertEqual(HTTPResponse.statusCode, 200, "HTTP response status code should be 200")
+
+            } else {
+                XCTFail("Response was not NSHTTPURLResponse")
+            }
+            
+            expectation.fulfill()
+        }
+        
+        task.resume()
+        
+        waitForExpectationsWithTimeout(task.originalRequest!.timeoutInterval) { error in
+            if let error = error {
+                print("Error: \(error.localizedDescription)")
+                XCTFail("There is an Error")
+
+            }
+            task.cancel()
+        }
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
+    
+    
+    func testPhotosURLMimeTye() {
+        
+        let URL = NSURL(string: "http://placehold.it/150/30ac17")!
+        
+        let expectation = expectationWithDescription("Test Album Photos MIME Type")
+        
+        let session = NSURLSession.sharedSession()
+        
+        let task = session.dataTaskWithURL(URL) { data, response, error in
+            
+            XCTAssertNotNil(data, "Data should not be nil")
+            
+            XCTAssertNil(error, "error should be nil")
+            
+            if let HTTPResponse = response as? NSHTTPURLResponse,
+                responseURL = HTTPResponse.URL,
+                MIMEType = HTTPResponse.MIMEType
+            {
+
+                XCTAssertNotEqual(responseURL.absoluteString, URL.absoluteString, "HTTP response URL should NOT be equal to original URL, Must Be Redirected to valid Image URL")
+
+                XCTAssertEqual(HTTPResponse.statusCode, 200, "HTTP response status code should be 200")
+                
+                XCTAssertEqual(MIMEType, "image/png", "HTTP response content type should be image/png")
+            } else {
+                XCTFail("Response was not NSHTTPURLResponse")
+            }
+            
+            expectation.fulfill()
+        }
+        
+        task.resume()
+        
+        waitForExpectationsWithTimeout(task.originalRequest!.timeoutInterval) { error in
+            if let error = error {
+                print("Error: \(error.localizedDescription)")
+            }
+            task.cancel()
         }
     }
     
